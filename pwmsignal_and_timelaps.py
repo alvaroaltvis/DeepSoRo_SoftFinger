343b6f32ca35ad5f113b9e6ed88c91d817192d2b
Alvaro Altamirano Visoso <65615594+alvaroaltvis@users.noreply.github.com>
2022-06-30 04:52:03 -0900

    Changed to the proper code, previously had the time laps

--- pwmsignal_and_timelaps.py
+++ pwmsignal_and_timelaps.py
@@ -1,35 +1,25 @@
-import os, sys
-import subprocess
+import RPi.GPIO as GPIO
+from time import sleep
+from camera_bash import *
 
-def pictures():
-    os.chdir(sys.path[0])
-    
-    for i in range(3):
-        x1 = 'raspi-gpio set 4 op'
-        x2 = 'raspi-gpio set 17 op'
-        
-        os.system(x1)
-        os.system(x2)
-        # select cam 1
-        x3 = 'i2cset -y 1 0x70 0x00 0x01'
-        x4 = 'raspi-gpio set 17 dl' #set the gpio17 low
-        x5 = 'raspi-gpio set  4 dl' #set the gpio4 low
-        os.system(x3)
-        os.system(x4)
-        os.system(x5)
-        bash_command = f'libcamera-still -o camera{i}.jpg'
-        process = subprocess.Popen(bash_command.split(), stdout=subprocess.PIPE)
-        output, error = process.communicate()
-    
-        # select cam 2
-        x6 = 'i2cset -y 1 0x70 0x00 0x02'
-        x7 = 'raspi-gpio set 17 dl' #set the gpio17 low
-        x8 = 'raspi-gpio set  4 dh' #set the gpio4 high
-        os.system(x6)
-        os.system(x7)
-        os.system(x8)
-        bash_command = f'libcamera-still -o camera_two{i}.jpg'
-        process = subprocess.Popen(bash_command.split(), stdout=subprocess.PIPE)
-        output, error = process.communicate()
-     
-    print('Finished')
+#Code for the pump
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(12,GPIO.OUT)
+
+pwm = GPIO.PWM(12,100)
+pwm.start(0)
+
+stopit = False
+
+while(stopit!= True):
+    dutycycle = input("Enter the percentage of Duty Cycle, 100 max value, 1 to quit: ")
+    if int(dutycycle) == 1:
+        stopit = True
+        break
+    #name = input("Enter the name of the pictures: ")
+    pwm.ChangeDutyCycle(int(dutycycle))
+    pictures()
+   
+GPIO.cleanup()
+print("Finish")
