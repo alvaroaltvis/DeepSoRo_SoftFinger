c4feacf91c60d3d3269c52836a46b60921ae04dd
Alvaro Altamirano Visoso <65615594+alvaroaltvis@users.noreply.github.com>
2022-06-30 04:52:52 -0900

    Code uses shell commands
    Takes two pictures using two camera modules

--- timelaps.py
+++ timelaps.py
@@ -1,18 +1,36 @@
-from picamera2 import Picamera2, Preview
-from time import sleep
+import os, sys
+import subprocess
 
-def picture(x):
-    picam2 = Picamera2()
-    preview_config = picam2.preview_configuration(main={"size": (640, 480)})
-    picam2.configure(preview_config)
-
-    picam2.start_preview(Preview.QTGL)
-    picam2.start()
-    sleep(2)
+def pictures():
+    os.chdir(sys.path[0])
     
-    for picture in range(10):
-        metadata = picam2.capture_file("/home/rasberrypi/Desktop/" + x + "{0:04d}.jpg".format(picture))
-        sleep(0.10)
+    for i in range(3):
+        x1 = 'raspi-gpio set 4 op'
+        x2 = 'raspi-gpio set 17 op'
         
-    picam2.close() 
-    print("Done")
+        os.system(x1)
+        os.system(x2)
+        # select cam 1
+        x3 = 'i2cset -y 1 0x70 0x00 0x01'
+        x4 = 'raspi-gpio set 17 dl' #set the gpio17 low
+        x5 = 'raspi-gpio set  4 dl' #set the gpio4 low
+        os.system(x3)
+        os.system(x4)
+        os.system(x5)
+        bash_command = f'libcamera-still -o camera{i}.jpg'
+        process = subprocess.Popen(bash_command.split(), stdout=subprocess.PIPE)
+        output, error = process.communicate()
+    
+        # select cam 2
+        x6 = 'i2cset -y 1 0x70 0x00 0x02'
+        x7 = 'raspi-gpio set 17 dl' #set the gpio17 low
+        x8 = 'raspi-gpio set  4 dh' #set the gpio4 high
+        os.system(x6)
+        os.system(x7)
+        os.system(x8)
+        bash_command = f'libcamera-still -o camera_two{i}.jpg'
+        process = subprocess.Popen(bash_command.split(), stdout=subprocess.PIPE)
+        output, error = process.communicate()
+     
+    print('Finished')
+
