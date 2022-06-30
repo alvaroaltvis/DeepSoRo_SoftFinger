b9516f781063b866d815ccf79d96e4fd0cfb9382
Alvaro Altamirano Visoso <65615594+alvaroaltvis@users.noreply.github.com>
2022-06-29 23:02:01 -0400

    Create timelaps.py

---
+++ timelaps.py
@@ -0,0 +1,18 @@
+from picamera2 import Picamera2, Preview
+from time import sleep
+
+def picture(x):
+    picam2 = Picamera2()
+    preview_config = picam2.preview_configuration(main={"size": (640, 480)})
+    picam2.configure(preview_config)
+
+    picam2.start_preview(Preview.QTGL)
+    picam2.start()
+    sleep(2)
+    
+    for picture in range(10):
+        metadata = picam2.capture_file("/home/rasberrypi/Desktop/" + x + "{0:04d}.jpg".format(picture))
+        sleep(0.10)
+        
+    picam2.close() 
+    print("Done")
