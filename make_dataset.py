40189d5407f05eafc4caf05571b583c5c703b201
Alvaro Altamirano Visoso <65615594+alvaroaltvis@users.noreply.github.com>
2022-08-01 19:13:19 -0400

    Update make_dataset.py

--- make_dataset.py
+++ make_dataset.py
@@ -15,13 +15,13 @@ DEBUG = False
 
 class MAKE_DATASET():
     
-    def __init__(self, sim_folder):
+    def __init__(self):
         # parameters
-        OUTPUT_PREFIX = '/home/nuc/Desktop/kinect_camera/DATASET'
+        OUTPUT_PREFIX = f'/home/nuc/Desktop/kinect_camera/DATASET/{folder_name}'
 
         # prepare path for file reading
-        self.image_path = os.path.join(sim_folder, 'Images')
-        self.pcd_path = os.path.join(sim_folder, 'PointCloud')
+        self.image_path = f"/home/nuc/Desktop/kinect_camera/DATA/{wanted_data}/Images"
+        self.pcd_path = f"/home/nuc/Desktop/kinect_camera/DATA/{wanted_data}/PointCloud"
         
         # check file length
         self.simulation_length = len(os.listdir(self.image_path))
@@ -49,19 +49,19 @@ class MAKE_DATASET():
 
             
         # create output path 
-        for i in range(101):
-            self.output_path = os.path.join(OUTPUT_PREFIX, os.path.basename(sim_folder)+f'_{i}.npz')
+        for i in range(10): # Adjust number to number of samples 
+            self.output_path = os.path.join(OUTPUT_PREFIX, f'_{i}.npz')
             np.savez(self.output_path, img=image_seq[i], pcd=pcd_seq[i])
         
     def load_files(self):
-        imgs_path = [os.path.join(self.image_path, f'{i}.jpg') for i in range(1, 102)] 
-        pcds_path = [os.path.join(self.pcd_path, f'data_{i}.npz') for i in range(1, 102)]
+        imgs_path = [os.path.join(self.image_path, f'{i}.jpg') for i in range(0, 10)] # Adjust number to number of samples 
+        pcds_path = [os.path.join(self.pcd_path, f'data_{i}.npz') for i in range(0, 10)] # Adjust number to number of samples
         
         # container to store data
         img_seq = np.zeros((self.simulation_length, 1, 256, 256))
         pcd_seq = np.zeros((self.simulation_length, 6216, 3))
         
-        for i in range(101):
+        for i in range(10): # Adjust number to number of samples 
             img_seq[i] = self.load_img_from_file_singlecam(imgs_path[i])
             pcd_seq[i] = self.load_pcd_from_file(pcds_path[i])
         return img_seq, pcd_seq
@@ -89,20 +89,36 @@ class MAKE_DATASET():
         #print(len(vertices))
         return vertices
     
-def parallel_worker(sim_folder):
-    # print('\n'+sim_folder)
-    MAKE_DATASET(sim_folder)
+# def parallel_worker(sim_folder):
+#     MAKE_DATASET(sim_folder)
 
     
 if __name__ == '__main__':
     
     os.chdir(sys.path[0])
-    
-    DATA_PATH = '/home/nuc/Desktop/kinect_camera/DATA'
-    
-    SIM_FOLDERS = []
-    for dir in os.listdir(DATA_PATH):
-        SIM_FOLDERS.append(os.path.abspath(os.path.join(DATA_PATH, dir)))
 
-    process_map(parallel_worker, SIM_FOLDERS, max_workers=24)
+    # Folder with data in it 
+    wanted_data = input("Choose the folder name where the data is comming from DATA_#: ")
+
+    # Create the final folder
+    folder_name = input("Folder for the DATASET -- DATASET_#: ")
+    parent_directory= "/home/nuc/Desktop/kinect_camera/DATASET"
+    path= os.path.join(parent_directory, folder_name)
+    os.mkdir(path)
+
+    # Download the remote data from raspi
+    # for i in range(10):  # Adjust number to number of samples 
+    #     subprocess.run(["scp", f"raspberrypi@192.168.1.2:/home/raspberrypi/Desktop/EmbededImages/{wanted_data}/{i}.jpg", f"/home/nuc/Desktop/kinect_camera/DATA/{wanted_data}/Images"])
+    
+    # Choose the folder from where the data if coming from 
+    DATA_PATH = f"/home/nuc/Desktop/kinect_camera/DATA/{wanted_data}" #Specify the folder I want 
+    
+    # SIM_FOLDERS = []
+    # for dir in os.listdir(DATA_PATH):
+    #     SIM_FOLDERS.append(os.path.abspath(os.path.join(DATA_PATH, dir)))
+
+    # process_map(parallel_worker, SIM_FOLDERS, max_workers=24)
+    combine_data = MAKE_DATASET()
+    combine_data.__init__()
+
     
