9916af6b53ca5d4b587258879791e03563176695
Alvaro Altamirano Visoso <65615594+alvaroaltvis@users.noreply.github.com>
2022-08-11 11:23:48 -0400

    Update make_dataset.py

--- make_dataset.py
+++ make_dataset.py
@@ -1,8 +1,10 @@
 import os, sys
 import numpy as np
+# import pymesh
 import cv2
 import multiprocessing as mp
 from scipy.spatial.transform import Rotation as R
+# from data_utils import *
 from tqdm.contrib.concurrent import process_map
 import open3d as o3d
 from random import seed
@@ -10,10 +12,15 @@ from random import randint
 from numpy.random import default_rng
 import subprocess
 
+## This file needs to be used in two ways. When first importing data it is important ot create the file DATASET_# manually. Then it is important to uncomment to see the length of the pointcloud. And after that we can crop them and send them to the DATASET 
+## Also change the ranges to upload every image
+
 DEBUG = False
 
+lenght = []
+
 class MAKE_DATASET():
-    
+
     def __init__(self):
         # parameters
         OUTPUT_PREFIX = f'/home/nuc/Desktop/kinect_camera/DATASET/{folder_name}'
@@ -31,7 +38,14 @@ class MAKE_DATASET():
 
         # load image and point cloud sequence files
         image_seq, pcd_seq = self.load_files()
-      
+
+        # image_seq = np.expand_dims(image_seq, axis=0)
+        # pcd_seq = np.expand_dims(pcd_b_seq, axis=0)
+        # com_coordinate_seq = np.expand_dims(com_coordinate_seq, axis=0)
+        # rotated_com_translation_seq = np.expand_dims(rotated_com_translation_seq, axis=0)
+        # com_rotation_seq = np.expand_dims(com_rotation_seq, axis=0)
+        # idx_seq = np.expand_dims(idx_seq, axis=0)       
+
         print(f'IMG Dim: {np.shape(image_seq)}')
         print(f'PCD Dim: {np.shape(pcd_seq)}')
         # print(f'IDX Dim: {np.shape(idx_seq)}')
@@ -41,19 +55,19 @@ class MAKE_DATASET():
 
             
         # create output path 
-        for i in range(10): # Adjust number to number of samples 
+        for i in range(300): # Adjust number to number of samples 
             self.output_path = os.path.join(OUTPUT_PREFIX, f'_{i}.npz')
             np.savez(self.output_path, img=image_seq[i], pcd=pcd_seq[i])
         
     def load_files(self):
-        imgs_path = [os.path.join(self.image_path, f'{i}.jpg') for i in range(0, 10)] # Adjust number to number of samples 
-        pcds_path = [os.path.join(self.pcd_path, f'data_{i}.npz') for i in range(0, 10)] # Adjust number to number of samples
+        imgs_path = [os.path.join(self.image_path, f'{i}.jpg') for i in range(0, 300)] # Adjust number to number of samples 
+        pcds_path = [os.path.join(self.pcd_path, f'data_{i}.npz') for i in range(0, 300)] # Adjust number to number of samples
         
         # container to store data
         img_seq = np.zeros((self.simulation_length, 1, 256, 256))
-        pcd_seq = np.zeros((self.simulation_length, 6216, 3))
+        pcd_seq = np.zeros((self.simulation_length, 4510, 3))  # Change depending on the size of the pointcloud 
         
-        for i in range(10): # Adjust number to number of samples 
+        for i in range(300): # Adjust number to number of samples 
             img_seq[i] = self.load_img_from_file_singlecam(imgs_path[i])
             pcd_seq[i] = self.load_pcd_from_file(pcds_path[i])
         return img_seq, pcd_seq
@@ -71,15 +85,22 @@ class MAKE_DATASET():
         # get vertices from mesh
         vertices = data['pcd']
 
-        # Downsize pointcloud
-        #print(len(vertices)) #-- See what the min points in a pointcloud is. This case: 6216 points 
+        # Check min size of pointcloud Downsize pointcloud
+        # print(len(vertices)) #-- See what the min points in a pointcloud is. Do this before eliminating the extra points
+        # lenght.append(len(vertices)) # Uncomment print(min(lent)) in the name function to see the minimu value 
+
+        # Eliminate the extra point in each pointcloud
         rng = default_rng()
-        extra_points=len(vertices) - 6216
+        extra_points=len(vertices) - 4510 # The number needs to be changed to what the len(vertices) gives on the commands above
         ran = range(0, len(vertices)-1)
         eliminate=rng.choice(ran, size=extra_points, replace=False)
         vertices = np.delete(vertices, eliminate, 0)
         #print(len(vertices))
         return vertices
+    
+# def parallel_worker(sim_folder):
+#     MAKE_DATASET(sim_folder)
+
     
 if __name__ == '__main__':
     
@@ -94,15 +115,21 @@ if __name__ == '__main__':
     path= os.path.join(parent_directory, folder_name)
     os.mkdir(path)
 
-    # Download the remote data from raspi
-    for i in range(10):  # Adjust number to number of samples 
+    # # Download the remote data from raspi
+    for i in range(300):  # Adjust number to number of samples 
         subprocess.run(["scp", f"raspberrypi@192.168.1.2:/home/raspberrypi/Desktop/EmbededImages/{wanted_data}/{i}.jpg", f"/home/nuc/Desktop/kinect_camera/DATA/{wanted_data}/Images"])
     
     # Choose the folder from where the data if coming from 
     DATA_PATH = f"/home/nuc/Desktop/kinect_camera/DATA/{wanted_data}" #Specify the folder I want 
+    
+    # SIM_FOLDERS = []
+    # for dir in os.listdir(DATA_PATH):
+    #     SIM_FOLDERS.append(os.path.abspath(os.path.join(DATA_PATH, dir)))
 
     # process_map(parallel_worker, SIM_FOLDERS, max_workers=24)
+
     combine_data = MAKE_DATASET()
     combine_data.__init__()
+    # print(min(lenght))
 
     
